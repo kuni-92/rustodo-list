618a74208cc3605225daeb7233ea9296df42f262
kunikuni03 <49485046+kunikuni03@users.noreply.github.com>
2022-01-02 16:00:23 +0900

    db write todolist

--- src/handler/todo.rs
+++ src/handler/todo.rs
@@ -1,3 +1,4 @@
+use rusqlite::{Connection, Result, params};
 use serde::Deserialize;
 
 #[derive(Deserialize, Debug)]
@@ -10,4 +11,20 @@ impl ToDo {
     pub fn new(content: String) -> Self {
         ToDo{content, finished: false}
     }
+
+    pub fn write_to_db(todo: &ToDo) -> Result<()> {
+        let finished = if todo.finished {0} else {1};
+
+        let conn = connect_to_db()?;
+        conn.execute(
+            "INSERT INTO todolist(content, finished) VALUES (?1, ?2)",
+            params![todo.content, finished]
+        )?;
+        Ok(())
+    }
+}
+
+
+fn connect_to_db() -> Result<Connection> {
+    Connection::open("./todolist.sqlite3")
 }
